905e4764b2d83540502b0e1167e834a74857d3e8
hudsonchurch <churchhud@gmail.com>
2026-03-15 20:33:42 -0700

    Sync latest funnel into app/public and bust iframe cache

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <iframe
       title="MatchFrame Funnel"
-      src="/matchframe-funnel.html"
+      src="/matchframe-funnel.html?v=20260315-2033"
       style={{ width: "100vw", height: "100vh", border: "none", display: "block" }}
     />
   );
