e4d23faa806ad1948af7e80fb3209a7f0fb1830e
hudsonchurch <churchhud@gmail.com>
2026-03-19 07:47:09 -0700

    Fix Cloudflare deploy: include media assets and remove horizontal overflow

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,8 +4,8 @@ export default function App() {
   return (
     <iframe
       title="MatchFrame Funnel"
-      src="/matchframe-funnel.html?v=20260315-2033"
-      style={{ width: "100vw", height: "100vh", border: "none", display: "block" }}
+      src="/matchframe-funnel.html?v=20260319-0748"
+      style={{ width: "100%", height: "100dvh", border: "none", display: "block" }}
     />
   );
 }
