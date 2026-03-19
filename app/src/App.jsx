a185286f90a9158fb02eb56c4b828ffe9ef47a76
hudsonchurch <churchhud@gmail.com>
2026-03-19 14:32:10 -0700

    Pass query/hash into funnel iframe for step deep links

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 export default function App() {
+  const qs = window.location.search || "";
+  const hash = window.location.hash || "";
+  const src = `/matchframe-funnel.html?v=20260319-1432${qs}${hash}`;
+
   return (
     <iframe
       title="MatchFrame Funnel"
-      src="/matchframe-funnel.html?v=20260319-0748"
+      src={src}
       style={{ width: "100%", height: "100dvh", border: "none", display: "block" }}
     />
   );
